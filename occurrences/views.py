387e5522ea7c1334c90c3b5672f1d629d2e20ac3
Antonio Ladeia <contato@antonioladeia.com>
2017-05-07 05:30:33 -0300

    testes sem  acentos

--- occurrences/views.py
+++ occurrences/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Abra a sua ocorrência")
+    return HttpResponse("Abra a sua ocorrencia")
