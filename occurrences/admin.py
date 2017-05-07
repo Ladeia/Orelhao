57d38abc96af70b3387b880bd21ea490c7f1303d
Antonio Ladeia <contato@antonioladeia.com>
2017-05-07 05:59:26 -0300

    create a model

--- occurrences/admin.py
+++ occurrences/admin.py
@@ -1,5 +1,3 @@
-import views
 from django.contrib import admin
-from models import *
 
 # Register your models here.
--- occurrences/models.py
+++ occurrences/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class OccurrenceType(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=300, blank=True)
