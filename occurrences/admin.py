e147ba7fc7a976ab437094cb8541baae5f7f557e
Antonio Ladeia <contato@antonioladeia.com>
2017-05-07 06:28:29 -0300

    ajustes no django admin

--- occurrences/admin.py
+++ occurrences/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import OccurrenceType
 
-admin.site.register(OccurrenceType)
-
 class OccurrenceTypeAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
+
+    def __unicode__(self):
+        return self.title; 
+
+admin.site.register(OccurrenceTypeAdmin)
