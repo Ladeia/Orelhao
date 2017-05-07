58c3951a1457cf2114dd1ad51a7753ca41dffcce
Antonio Ladeia <contato@antonioladeia.com>
2017-05-07 06:22:50 -0300

    alterando formas de visualização no admin

--- occurrences/admin.py
+++ occurrences/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import OccurrenceType
 
 admin.site.register(OccurrenceType)
+
+class OccurrenceTypeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
